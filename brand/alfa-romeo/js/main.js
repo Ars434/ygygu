0b579f389f73f14e77027a0a38c5cb458d5bb82e
Ars434 <108485592+Ars434@users.noreply.github.com>
2025-03-11 02:03:53 +0300

    Update main.js

--- brand/alfa-romeo/js/main.js
+++ brand/alfa-romeo/js/main.js
@@ -297,7 +297,7 @@
             {
                 name: 'Tipo 316',
                 year: '1938-1939',
-                image: '../../img/AlfaRomeoTipo316.jpg',
+                image: '../../img/AlfaRomeoTipo316.JPG',
             },
             {
                 name: 'Tipo 308',
